6121334ac1d6bff17c752e00d2cf922839adccc5
Mathijs Moerman <mathijs.moerman@qrama.io>
2016-12-23 16:24:36 +0100

    Updated structure, not finished

--- reactive/controller-gce.py
+++ reactive/controller-gce.py
@@ -27,7 +27,7 @@ from charmhelpers.core.host import service_restart
 @when_not('controller-gce.installed')
 def install():
     api_dir = requests.get('http://localhost:5000').json()['message']['api_dir']
-    copyfile('{}/files/controller_gce.py'.format(charm_dir()), '{}/api/controller_gce.py'.format(api_dir))
+    copyfile('{}/files/controller_gce.py'.format(charm_dir()), '{}/controllers/controller_gce.py'.format(api_dir))
     service_restart('sojobo-api')
     status_set('active', 'data copied')
     set_state('controller-gce.installed')
